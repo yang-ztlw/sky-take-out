364a2c202458daa0816a2c79f460fc175e312cc5
yang <3552986650@qq.com>
2025-07-17 20:56:47 +0800

    新增员工业务代码开发

--- sky-pojo/src/main/java/com/sky/entity/Employee.java
+++ sky-pojo/src/main/java/com/sky/entity/Employee.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Accessors(chain = true)
 public class Employee implements Serializable {
 
     private static final long serialVersionUID = 1L;
